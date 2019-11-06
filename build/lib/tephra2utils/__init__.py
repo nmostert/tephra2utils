f7b33db401a0e81cd91e7fe19c3f6e14bed99c5e
Mostert <nmostert@massey.ac.nz>
2019-11-06 13:42:31 +1300

    setup added

---
+++ build/lib/tephra2utils/__init__.py
@@ -0,0 +1,2 @@
+def placeholder():
+    return ('This package will contain utilities for reading and writing TEPHRA2 files, visualization, statistical analysis, etc.')
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='tephra2utils',
+      version='0.1',
+      description='This package will contain utilities for reading and writing TEPHRA2 files, visualization, statistical analysis, etc.',
+      url='https://github.com/nmostert/tephra2utils.git',
+      author='Nic Mostert',
+      author_email='nicmos@protonmail.com',
+      license='MIT',
+      packages=['tephra2utils'],
+      zip_safe=False)
